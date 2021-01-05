4e471772d57582f0b694c3ac733e0963af7b885e
Tibuh <vitortiburtino10@gmail.com>
2021-01-05 20:14:39 -0300

    Domain model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity 
+@Table(name = "tb_order")
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+@ToString @EqualsAndHashCode(of = {"id"})
+public class Order implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Setter
+	private Long id;
+	@Setter
+	private String address;
+	@Setter
+	private Double latitude;
+	@Setter
+	private Double longitude;
+	@Setter
+	private Instant moment;
+	@Setter
+	private OrderStatus status;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+		joinColumns = @JoinColumn(name = "order_id"), 
+		inverseJoinColumns = @JoinColumn(name = "product_id"))
+	private  Set<Product> products = new HashSet<>();
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dsdeliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity 
+@Table(name = "tb_product")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString @EqualsAndHashCode(of = {"id"})
+public class Product  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+}
