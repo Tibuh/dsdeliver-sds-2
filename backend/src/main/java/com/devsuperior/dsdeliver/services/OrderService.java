33ce535c2a47e9f2c4c7630120a4c028e3f2d487
Tibuh <vitortiburtino10@gmail.com>
2021-01-05 22:46:32 -0300

    POST /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -43,4 +43,14 @@ public class OrderService {
 		order = orderRepository.save(order);
 		return new OrderDto(order);
 	};
+	
+	@Transactional
+	public OrderDto setDelivered(Long id){
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
+		
+		return new OrderDto(order);
+
+	};
 }
